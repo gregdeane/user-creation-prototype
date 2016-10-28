e8a4acc529e1aa461f90cd40708526894ab3198b
Greg Deane <greg.james.deane@zalando.de>
2016-10-28 12:58:55 +0200

    refactor(layout): adjusting layout

--- src/components/user/UserForm.jsx
+++ src/components/user/UserForm.jsx
@@ -5,45 +5,72 @@ import ButtonInput from '../common/ButtonInput';
 const UserForm = ({ user, onSave, onChange, loading, errors }) => {
   return(
     <div className="dc-row dc-block-grid--small-1 dc-block-grid--medium-2 dc-block-grid--large-3">
-      <div className="dc-column">
-        <h1>Manage User</h1>
-        <form>
-          <TextInput name="firstName"
-                     label="First Name"
-                     placeholder="First Name"
-                     value={user.firstName}
-                     onChange={onChange}
-                     errors={errors.firstName} />
+      <div className="dc-column temp-border">
+        <h1>New User</h1>
+        <TextInput name="firstName"
+                   label="First Name"
+                   placeholder="First Name"
+                   value={user.firstName}
+                   onChange={onChange}
+                   errors={errors.firstName} />
 
-          <TextInput name="lastName"
-                     label="Last Name"
-                     placeholder="Last Name"
-                     value={user.lastName}
-                     onChange={onChange}
-                     errors={errors.lastName} />
+        <TextInput name="lastName"
+                   label="Last Name"
+                   placeholder="Last Name"
+                   value={user.lastName}
+                   onChange={onChange}
+                   errors={errors.lastName} />
 
-          <TextInput name="email"
-                     label="Email"
-                     placeholder="Email"
-                     value={user.email}
-                     onChange={onChange}
-                     errors={errors.email} />
+        <TextInput name="email"
+                   label="Email"
+                   placeholder="Email"
+                   value={user.email}
+                   onChange={onChange}
+                   errors={errors.email} />
 
-          <ButtonInput type="submit"
-                       disabled={loading}
-                       label={(loading && 'Saving...') || 'Save'}
-                       onClick={onSave} />
-        </form>
+        <ButtonInput type="submit"
+                     disabled={loading}
+                     label='Next'
+                     onClick={onSave} />
       </div>
-      <div className="dc-column">
-        <input type="checkbox" className="dc-checkbox" id="c1" />
-        <label htmlFor="c1" className="dc-label">Check this</label>
+      <div className="dc-column temp-border">
+        <h1>Extra Permissions</h1>
+        <ul>
+          <li>
+            <input type="checkbox" className="dc-checkbox" id="works-at-zalando" />
+            <label htmlFor="works-at-zalando" className="dc-label">Works at Zalando</label>
+            <div className="dc-label--help-text">Can view, edit, and create internal Zalando users</div>
+          </li>
+          <li>
+            <input type="checkbox" className="dc-checkbox" id="all-partners-and-apps" />
+            <label htmlFor="all-partners-and-apps" className="dc-label">All business partners & apps</label>
+            <div className="dc-label--help-text">This user will receive all new business partner and apps</div>
+          </li>
+          <li>
+            <input type="checkbox" className="dc-checkbox" id="team-norris" />
+            <label htmlFor="team-norris" className="dc-label">Team Norris</label>
+            <div className="dc-label--help-text">This user will have Chuck Norris's powers and beauty</div>
+          </li>
+          <li>
+            <input type="checkbox" className="dc-checkbox" id="user-management" />
+            <label htmlFor="user-management" className="dc-label">User Management</label>
+            <div className="dc-label--help-text">This user willl be able to create, edit and delete users</div>
+          </li>
+        </ul>
       </div>
-      <div className="dc-column">
-        <input type="checkbox" id="c4" className="dc-checkbox dc-checkbox--alt" />
-        <label htmlFor="c4" className="dc-label">Check this</label>
-        <input type="checkbox" id="c5" className="dc-checkbox dc-checkbox--alt" defaultChecked />
-        <label htmlFor="c5" className="dc-label">Check that</label>
+      <div className="dc-column temp-border hide">
+        <h1>Business Partners & Apps</h1>
+        <TextInput name="search"
+                   placeholder="Add a business partner..."
+                   onChange={onChange} />
+      </div>
+      <div className="dc-column temp-border">
+        <h1>Applications</h1>
+        <ul className="dc-list dc-list--is-scrollable sg-icons">
+          <li className="dc-list__item dc-list__item--is-interactive">CMS</li>
+          <li className="dc-list__item dc-list__item--is-interactive">Analytics</li>
+          <li className="dc-list__item dc-list__item--is-interactive">ACT</li>
+        </ul>
       </div>
     </div>
   );
